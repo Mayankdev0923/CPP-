6678266041fbe7e65d753b4a3a6130f8b57a98dc
Mayankdev0923 <mayankdev0923@gmail.com>
2025-09-06 13:24:15 +0530

    I comleted backlog for notes, & i got to create my own function for lastoccurence in recursion 1

--- RECURSION1/lastOccurence.cpp
+++ RECURSION1/lastOccurence.cpp
@@ -13,7 +13,16 @@ int lastOccurence(vector<int> vec, int target, int i){
     return lastOccurence(vec, target, i+1);
 }
 
+int lo(vector<int> arr, int target, int n){
+    if(n==-1 || arr[n] == target){
+        return n;
+    }
+
+    return lo(arr, target, n-1);
+}
+
 int main(){
     vector<int> v1 = {0,3,4,5,66,2,5,1,66,-1};
     cout<<lastOccurence(v1, 66, 0);
+    cout<<endl<<lo(v1, 66, v1.size()-1);
 }
